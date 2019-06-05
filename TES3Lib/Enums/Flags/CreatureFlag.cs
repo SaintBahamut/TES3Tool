52ff7f59a14af41db5711000d7e7e378cf29f552
NullCascade <nullcascade@gmail.com>
2019-06-04 18:53:27 -0700

    Fix creatures having default flags forced on them.

--- TES3Lib/Enums/Flags/CreatureFlag.cs
+++ TES3Lib/Enums/Flags/CreatureFlag.cs
@@ -9,7 +9,6 @@
         Swims = 0x0010,
         Flies = 0x0020,
         Walks = 0x0040,
-        DefaultFlags = 0x0048,
         Essential = 0x0080,
         SkeletonBlood = 0x0400,
         MetalBlood = 0x0800
